99a0a563c44cc80d57d34bd834cac5dffdb3f3d4
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-11-17 07:31:43 +0000

    added link

--- provider_app/src/components/Pannel.js
+++ provider_app/src/components/Pannel.js
@@ -3,7 +3,7 @@ import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
 import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
-
+import AddBoxIcon from '@material-ui/icons/AddBox';
 function Pannel() {
   return (
     <div className="z-30 inset-y-0 left-0 w-46 sm:hidden md:block bg-gray-900 border-r overflow-y-auto sm:translate-x-0 sm:transition-none -translate-x-full ease-in transition-medium">
@@ -12,6 +12,10 @@ function Pannel() {
           <AccessibilityNewIcon className="mx-2" />
           <span className="px-3 py-1 sm:hidden md:block">Assistance</span>
         </button>
+        <a className="block text-gray-300 w-36 flex justify-between items-center hover:text-red-100 my-4 rounded-lg px-2 py-1 hover:bg-gray-700" href="https://passanger.netlify.com/">
+          <AddBoxIcon className="mx-2" />
+          <span className="px-3 py-1 sm:hidden md:block">Create Request</span>
+        </a>
         <button className="block text-gray-300 w-36 flex justify-between items-center hover:text-red-100 my-4 rounded-lg px-2 py-1 hover:bg-gray-700">
           <SupervisorAccountIcon className="mx-2" />
           <span className="px-3 py-1 sm:hidden md:block">Employes</span>
@@ -25,6 +29,10 @@ function Pannel() {
           <SettingsApplicationsIcon className="mx-2" />
           <span className="px-3 py-1 sm:hidden md:block">Settings</span>
         </button>
+        <a className="block text-gray-300 w-36 flex justify-between items-center hover:text-red-100 my-4 rounded-lg px-2 py-1 hover:bg-gray-700">
+          <AddBoxIcon className="mx-2" />
+          <span className="px-3 py-1 sm:hidden md:block">Create Request</span>
+        </a>
       </div>
     </div>
   )
